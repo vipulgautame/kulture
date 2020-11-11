341d953262e246f2ce1d8ff08fc3dcfe00221236
unknown <vipulgautame123987@gmail.com>
2020-11-11 19:52:31 +1100

    margins changed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import smoothscroll from 'smoothscroll-polyfill';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+smoothscroll.polyfill();
 if (environment.production) {
   enableProdMode();
 }
